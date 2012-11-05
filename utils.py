28e9230132e7ed1f12461019c41ff9b73c9c07a5
Marcin Wielgoszewski <marcin.wielgoszewski@gmail.com>
2012-11-05 13:20:16 -0500

    PEP-8, docstrings

--- utils.py
+++ utils.py
@@ -3,14 +3,18 @@ from base64 import urlsafe_b64decode, urlsafe_b64encode
 
 
 def dotnet_b64decode(s):
+    '''Decode .NET Web-Base64 encoded data.'''
     s, pad_bytes = s[:-1], int(s[-1])
     s += ('=' * pad_bytes)
     return urlsafe_b64decode(s)
 
+
 def dotnet_b64encode(s):
+    '''.NET Web-Base64 encode data.'''
     s = urlsafe_b64encode(s)
     pad_bytes = s.count('=')
     return s[:-pad_bytes or len(s)] + str(pad_bytes)
+
 
 def is_vulnerable(encrypted):
     '''
